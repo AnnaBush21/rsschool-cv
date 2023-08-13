d160820ab655a7f2dfb6a933ccbe8957d81d2ea2
AnnaBush21 <annabush13@yandex.ru>
2023-08-14 00:52:03 +0300

    feat:add burger to main library/index.html

--- library/index.js
+++ library/index.js
@@ -1,1 +1,6 @@
--
+const  header_burger_btn = document?.querySelector('[.burger]');
+const nav_menu = document?.querySelector('[.nav]');
+
+burger?.addEventListener('click', () => {
+nav.classList.toggle('nav--visible');
+});
